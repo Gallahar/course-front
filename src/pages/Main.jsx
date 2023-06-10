e0108d4ea6ee721d10167299cb88908814cb886b
David <david.kushnir.d@gmail.com>
2023-06-10 20:05:48 +0300

    models

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -4,13 +4,13 @@ import { Suspense, useContext } from 'react'
 import { MainContext } from 'providers/MainProvider'
 
 const Etilen = () => {
-	const Etilen = useFBX('../../3d-models/Etilen.fbx')
+	const Etilen = useFBX('/3d-models/Etilen.fbx')
 
 	return <primitive style={{}} object={Etilen} scale={1.5} />
 }
 
 const Etan = () => {
-	const Etan = useFBX('../../3d-models/Etan.fbx')
+	const Etan = useFBX('/3d-models/Etan.fbx')
 
 	return <primitive object={Etan} scale={1.5} />
 }
@@ -18,22 +18,28 @@ const Etan = () => {
 export const Main = () => {
 	return (
 		<section>
-			<div className='container'>
-				<h1>Courses.ru - лучшая образовательная платформа для студентов!</h1>
-				<div className='main_info'>
-					<p className='main_heading'>На нашем ресурсе вы найдете</p>
-					<div className='main_list'>
-						<p className='main_list_item'>- более 1000 курсов</p>
-						<p className='main_list_item'>- более 500 тестов</p>
-						<p className='main_list_item'>- удобную систему оценивания</p>
-						<p className='main_list_item'>- круглосуточную поддержку</p>
+			<div className="container">
+				<h1>
+					Courses.ru - лучшая образовательная платформа для студентов!
+				</h1>
+				<div className="main_info">
+					<p className="main_heading">На нашем ресурсе вы найдете</p>
+					<div className="main_list">
+						<p className="main_list_item">- более 1000 курсов</p>
+						<p className="main_list_item">- более 500 тестов</p>
+						<p className="main_list_item">
+							- удобную систему оценивания
+						</p>
+						<p className="main_list_item">
+							- круглосуточную поддержку
+						</p>
 					</div>
 				</div>
-				<div className='models_wrapper'>
-					<div className='model_wrapper'>
-						<p className='model_name'>Этилен - C2H4</p>
+				<div className="models_wrapper">
+					<div className="model_wrapper">
+						<p className="model_name">Этилен - C2H4</p>
 						<div>
-							<Canvas className='main_canvas'>
+							<Canvas className="main_canvas">
 								<Suspense fallback={null}>
 									<Etilen />
 									<OrbitControls />
@@ -41,10 +47,10 @@ export const Main = () => {
 							</Canvas>
 						</div>
 					</div>
-					<div className='model_wrapper'>
-						<p className='model_name'>Этан - C2H6</p>
+					<div className="model_wrapper">
+						<p className="model_name">Этан - C2H6</p>
 						<div>
-							<Canvas className='main_canvas'>
+							<Canvas className="main_canvas">
 								<Suspense fallback={null}>
 									<Etan />
 									<OrbitControls />
