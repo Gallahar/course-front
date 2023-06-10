964abf79b4ab641f4fdb868451cb4d862ba96235
David <david.kushnir.d@gmail.com>
2023-06-10 20:38:39 +0300

    rt

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -5,14 +5,14 @@ import { Login } from 'pages/Login'
 import { Register } from 'pages/Register'
 import { ProtectedUserRoute } from './ProtectedUserRoute'
 import { ProtectedAdminRoute } from './ProtectedAdminRoute'
-import { Courses } from 'pages/manage/read/Courses'
-import { Tests } from 'pages/manage/read/Tests'
+import { Courses } from 'pages/Courses'
+import { Tests } from 'pages/Tests'
 import { UserTest } from 'pages/UserTest'
-import { CourseStatistics } from 'pages/manage/read/CourseStatistics'
-import { TestStatistics } from 'pages/manage/read/TestStatistics'
-import { EditCourse } from 'pages/manage/edit/EditCourse'
+import { CourseStatistics } from 'pages/CourseStatistics'
+import { TestStatistics } from 'pages/TestStatistics'
+import { EditCourse } from 'pages/EditCourse'
 import { UserCourse } from 'pages/UserCourse'
-import { EditTest } from 'pages/manage/edit/EditTest'
+import { EditTest } from 'pages/EditTest'
 import { Main } from 'pages/Main'
 import { UserTests } from 'pages/UserTests'
 import { UserCourses } from 'pages/UserCourses'
@@ -27,20 +27,19 @@ export const router = createBrowserRouter([
 				<AdminLayout />
 			</ProtectedAdminRoute>
 		),
-		path: '/manage',
 		children: [
 			{
 				element: <Courses />,
-				path: 'courses',
+				path: 'manage/courses',
 			},
-			{ element: <CourseStatistics />, path: 'courses/:id' },
-			{ element: <EditCourse />, path: 'courses/edit/:id' },
+			{ element: <CourseStatistics />, path: 'manage/courses/:id' },
+			{ element: <EditCourse />, path: 'manage/courses/edit/:id' },
 			{
 				element: <Tests />,
 				path: 'tests',
 			},
-			{ element: <TestStatistics />, path: 'tests/:id' },
-			{ element: <EditTest />, path: 'tests/edit/:id' },
+			{ element: <TestStatistics />, path: 'manage/tests/:id' },
+			{ element: <EditTest />, path: 'manage/tests/edit/:id' },
 		],
 	},
 	{
